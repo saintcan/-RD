8f41ed9d9531e201af5bf9c7788adf65b3b49e12
saintcan <43911700+saintcan@users.noreply.github.com>
2024-02-25 11:42:08 +0100

    20

--- main.cpp
+++ main.cpp
@@ -1,30 +1,71 @@
 #include <iostream>
-#include "Character.h"
-#include "Weapon.h"
+#include <vector>
+
+// Абстрактний спостерігач
+class Observer {
+public:
+    virtual void update(float temperature, float humidity, float pressure) = 0;
+};
+
+// Абстрактний суб'єкт (видавець)
+class Subject {
+protected:
+    std::vector<Observer*> observers;
+
+public:
+    virtual void attach(Observer* observer) {
+        observers.push_back(observer);
+    }
+
+    virtual void detach(Observer* observer) {
+        observers.erase(std::remove(observers.begin(), observers.end(), observer), observers.end());
+    }
+
+    virtual void notify(float temperature, float humidity, float pressure) {
+        for (Observer* observer : observers) {
+            observer->update(temperature, humidity, pressure);
+        }
+    }
+};
+
+// Конкретний суб'єкт (конкретний видавець) - клас, що представляє погоду
+class WeatherData : public Subject {
+private:
+    float temperature;
+    float humidity;
+    float pressure;
+
+public:
+    void setMeasurements(float temp, float hum, float pres) {
+        temperature = temp;
+        humidity = hum;
+        pressure = pres;
+        notify(temperature, humidity, pressure);
+    }
+};
+
+// Конкретний спостерігач - відображення погоди на екрані
+class Display : public Observer {
+public:
+    void update(float temperature, float humidity, float pressure) override {
+        std::cout << "Current conditions: " << temperature << "C, " << humidity << "% humidity, " << pressure << "hPa pressure" << std::endl;
+    }
+};
 
 int main() {
-    Character character;
+    WeatherData weatherData;
 
-    std::unique_ptr<Weapon> swordPtr(new Sword());
-    std::unique_ptr<Weapon> bowPtr(new Bow());
-    std::unique_ptr<Weapon> spearPtr(new Spear());
-    std::unique_ptr<Weapon> fireStaffPtr(new FireStaff());
-    std::unique_ptr<Weapon> iceStaffPtr(new IceStaff());
+    Display display1;
+    Display display2;
 
-    character.SetWeapon(std::move(swordPtr)); // Передача власності розумного вказівника
-    character.SetWeapon(std::move(bowPtr));
-    character.SetWeapon(std::move(spearPtr));
-    character.SetWeapon(std::move(fireStaffPtr));
-    character.SetWeapon(std::move(iceStaffPtr));
+    weatherData.attach(&display1);
+    weatherData.attach(&display2);
 
-    Weapon* weapon = character.GetWeapon();
-    if (weapon) {
-        std::cout << "Character is using: " << weapon->GetName() << std::endl;
-        std::cout << "Damage: " << weapon->GetDamage() << std::endl;
-    }
-    else {
-        std::cout << "Character has no weapon assigned." << std::endl;
-    }
+    weatherData.setMeasurements(25.0f, 60.0f, 1013.0f);
+
+    weatherData.detach(&display1);
+
+    weatherData.setMeasurements(27.0f, 55.0f, 1010.0f);
 
     return 0;
 }
